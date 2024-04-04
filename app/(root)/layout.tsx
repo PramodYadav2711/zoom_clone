a87a4d7f0c59cc5a5fd32f25d98889dbe365ff08
Pramod Yadav <pramodyadavpy2711@gmail.com>
2024-04-04 19:03:56 +0530

    integrating Stream.io Video & audio

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,12 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider';
 import React, { ReactNode } from 'react';
 
 function RootLayout({ children }: { children: ReactNode }) {
-  return <main>{children}</main>;
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
 }
 
 export default RootLayout;
